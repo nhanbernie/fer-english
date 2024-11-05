f8535fb445894ca59840750c88116a130d92c45f
Huynh Tan Luc <tanluc.jdtech04@gmail.com>
2024-11-05 10:35:44 +0700

    Salary and Fee

--- ferenglish.client/src/modules/SalaryAndFee/page/SalaryAndFee.jsx
+++ ferenglish.client/src/modules/SalaryAndFee/page/SalaryAndFee.jsx
@@ -1,9 +1,14 @@
-
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/SalaryAndFee.scss";
+import Table from "../components/Table";
 
 const SalaryAndFee = () => {
   return (
-    <div>SalaryAndFee</div>
-  )
-}
+    <Container className="mt-5">
+      <Table />
+    </Container>
+  );
+};
 
 export default SalaryAndFee;
